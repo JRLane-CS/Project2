ad25e9223b04fff26a6e44800d2990c2a85779a5
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2019-11-25 14:32:07 -0600

    up

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ max: 10,
 idleTimeoutMillis: 30000,
 };
 const { Pool } = require('pg'); 
-const pool = new Pool(heroconfig);  
+const pool = new Pool(process.env.DATABASE_URL);  
 
 //set query variables
 var singleQuery = '';
